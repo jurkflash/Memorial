d49c2e927d1f110904748251623abe594b63f347
ABOD-ILLEGEAR1\wayne.lim_abod <jurkflash@hotmail.com>
2021-05-19 22:19:20 +0800

    Rename QuadrangleCentreIdToColumbariumCentreIdForColumbariumItemsTable

---
+++ Web/Memorial/Migrations/202105191416226_RenameQuadrangleCentreIdToColumbariumCentreIdForColumbariumItemsTable.cs
@@ -0,0 +1,20 @@
+﻿namespace Memorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameQuadrangleCentreIdToColumbariumCentreIdForColumbariumItemsTable : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.ColumbariumItems", name: "QuadrangleCentreId", newName: "ColumbariumCentreId");
+            RenameIndex(table: "dbo.ColumbariumItems", name: "IX_QuadrangleCentreId", newName: "IX_ColumbariumCentreId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.ColumbariumItems", name: "IX_ColumbariumCentreId", newName: "IX_QuadrangleCentreId");
+            RenameColumn(table: "dbo.ColumbariumItems", name: "ColumbariumCentreId", newName: "QuadrangleCentreId");
+        }
+    }
+}
