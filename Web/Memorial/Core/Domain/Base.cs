95a0061be6bf299f09bebecd19025a4bc322dd0e
ABOD-ILLEGEAR1\wayne.lim_abod <jurkflash@hotmail.com>
2021-12-30 23:43:28 +0800

    Add ActiveStatus to Base

--- Web/Memorial/Core/Domain/Base.cs
+++ Web/Memorial/Core/Domain/Base.cs
@@ -7,6 +7,8 @@ namespace Memorial.Core.Domain
 {
     public class Base
     {
+        public bool ActiveStatus { get; set; }
+
         public DateTime CreateDate { get; set; }
 
         public DateTime? ModifyDate { get; set; }
