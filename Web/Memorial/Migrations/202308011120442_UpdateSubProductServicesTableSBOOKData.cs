d968aa541433db18cad6a936f1d0fd6cad1de749
Wayne <jurkflash@hotmail.com>
2023-08-01 19:44:55 +0800

    Update sub product item

---
+++ Web/Memorial/Migrations/202308011120442_UpdateSubProductServicesTableSBOOKData.cs
@@ -0,0 +1,17 @@
+﻿namespace Memorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateSubProductServicesTableSBOOKData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE SubProductServices SET Name=N'租 Rent' WHERE Code='SBOOK'");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
